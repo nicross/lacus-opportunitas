c992e4220f56d747df796425423554086f3955d0
nicross <nicross@users.noreply.github.com>
2025-02-10 18:35:29 -0600

    Rebalance price of goods around the 8x method

--- src/js/content/goods/all.js
+++ src/js/content/goods/all.js
@@ -1,10 +1,6 @@
-//Agricultural
-content.goods.invent({
-  id: 'agricultural-equipment',
-  name: 'Agricultural equipment',
-  baseCost: 1024,
-})
+// Methodology: It takes eight sales to be able to afford the next tier
 
+//Agricultural
 content.goods.invent({
   id: 'essentials',
   name: 'Essentials',
@@ -19,69 +15,75 @@ content.goods.invent({
 
 // Extraction
 content.goods.invent({
-  id: 'extraction-equipment',
-  name: 'Extraction equipment',
-  baseCost: 512,
-})
-
-content.goods.invent({
   id: 'copper-ore',
   name: 'Copper ore',
-  baseCost: 2,
+  baseCost: 8 ** 1,
 })
 
 content.goods.invent({
   id: 'iron-ore',
   name: 'Iron ore',
-  baseCost: 8,
+  baseCost: 8 ** 2,
 })
 
 content.goods.invent({
   id: 'gold-ore',
   name: 'Gold ore',
-  baseCost: 32,
+  baseCost: 8 ** 3,
 })
 
 content.goods.invent({
   id: 'platinum-ore',
   name: 'Platinum ore',
-  baseCost: 128,
+  baseCost: 8 ** 4,
 })
 
 // Refinement
 content.goods.invent({
-  id: 'refinement-equipment',
-  name: 'Refinement equipment',
-  baseCost: 256,
-})
-
-content.goods.invent({
   id: 'copper-bars',
   name: 'Copper bars',
-  baseCost: 16,
+  baseCost: 8 ** 2,
 })
 
 content.goods.invent({
   id: 'iron-bars',
   name: 'Iron bars',
-  baseCost: 64,
+  baseCost: 8 ** 3,
 })
 
 content.goods.invent({
   id: 'gold-bars',
   name: 'Gold bars',
-  baseCost: 256,
+  baseCost: 8 ** 4,
 })
 
 content.goods.invent({
   id: 'platinum-bars',
   name: 'Platinum bars',
-  baseCost: 1024,
+  baseCost: 8 ** 5,
 })
 
-// Luxury
+// Manufacturing
+content.goods.invent({
+  id: 'agricultural-equipment',
+  name: 'Agricultural equipment',
+  baseCost: 8 ** 3,
+})
+
+content.goods.invent({
+  id: 'extraction-equipment',
+  name: 'Extraction equipment',
+  baseCost: 8 ** 4,
+})
+
+content.goods.invent({
+  id: 'refinement-equipment',
+  name: 'Refinement equipment',
+  baseCost: 8 ** 5,
+})
+
 content.goods.invent({
   id: 'consumer-goods',
   name: 'Consumer goods',
-  baseCost: 2048,
+  baseCost: 8 ** 6,
 })
--- src/js/content/goods/luxury.js
+++ src/js/content/goods/luxury.js
@@ -1,6 +1,9 @@
 content.goods.luxury = engine.fn.extend(content.goods.base, {
   getBaseCost: function () {
-    return engine.fn.srand('luxury', this.port, 'basePrice')(256, 768) * content.goods.priceMultiplier()
+    return this.getBaseCostRaw() * content.goods.priceMultiplier()
+  },
+  getBaseCostRaw: function () {
+    return 8 ** engine.fn.srand('luxury', this.port, 'basePrice')(3, 4)
   },
   getBuyCost: function () {
     return Math.round(this.getBaseCost())
@@ -9,9 +12,12 @@ content.goods.luxury = engine.fn.extend(content.goods.base, {
     return content.ports.get(this.port)
   },
   getSellCost: function (port) {
-    const base = this.getBaseCost(),
+    const base = this.getBaseCostRaw(),
       distance = engine.fn.distance(port, this.getPort())
 
-    return Math.round(base * (1 + (distance / 1000)))
+    // Up to 8^7 at optimal conditions
+    const inflated = base * (8 ** (0.5 + (distance / 1000 / 2)))
+
+    return Math.round(inflated * content.goods.priceMultiplier())
   },
 })
